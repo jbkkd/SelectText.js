967e5632efe19d358839ff079ffe44cfab5c459b
Omer Korner <omerkorner@gmail.com>
2014-11-19 18:57:47 +0200

    name refactoring, removed unused code

--- SelectText.js
+++ SelectText.js
@@ -1,6 +1,6 @@
 var i = 0;
 var intervalID;
-function iterateNodes(node) {
+function setHighlightInterval(node) {
     intervalID = setInterval(function() {highlight(node)}, 50);
     // highlight(node);
 }
@@ -18,14 +18,12 @@ function highlight(node) {
 
 function iterateNextNode(node, iterateChildren) {
     if (iterateChildren && node.childNodes.length > 0) {
-        // for (var j = 0; j < node.childNodes.length; j++) {
-            iterateNodes(node.childNodes[0]);
-            return;
-        // }
+        setHighlightInterval(node.childNodes[0]);
+        return;
     }
     if (node.nextSibling) {
         if (node.nextSibling.nodeType == 3) {
-            iterateNodes(node.nextSibling);
+            setHighlightInterval(node.nextSibling);
         } else {
             iterateNextNode(node.nextSibling, true);
         }
